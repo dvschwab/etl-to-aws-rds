515a03fd59d56886b3bb322dba32cf87a2d4739a
David Schwab <dvschwab@protonmail.com>
2021-08-26 15:28:22 -0400

    Added unit tests for file-handling and refactored extract_field.py into several more specialized functions instead of two large ones.

--- data_extract/extract_field.py
+++ data_extract/extract_field.py
@@ -1,8 +1,9 @@
 # Extracts a list of unique values for a given field in a delimited file
 # Optionally sorts the list
-# Intended usage is to extract dimension fields from a data file
-# so they can be loaded into a database table
-# DVS 7/27/2021
+# Intended to be run as a script to extract dimension fields from a data file
+# so they can be loaded into a database table, but the extract_delimited_filed()
+# function is general enough to have other uses
+# DVS 8/26/2021
 
 import csv
 import argparse
@@ -40,52 +41,107 @@ def extract_delimited_field(file_reader, position):
 
     return unique_field
 
-def extract_field_from_file():
-    
-    # Parse CLI args
-    
+def parse_cli_args():
+    """
+        Parses the CLI args if file is ran as a script.
+
+        :params: None
+        :returns: the populated namespace as an argparse.Namespace object
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help="The file to be processed")
     parser.add_argument("output_file", help="The output file")
     parser.add_argument("field_position", type=int, help="The numeric position of the field to be extracted (zero-based)")
-    parser.add_argument("-d", "--delimiter", choices=['tab', 'pipe', 'semi-colon'], help="The file delimiter of the input file")
-    parser.add_argument("-s", "--sorted", action = "store_true", help="Whether the output file should be sorted in ascendant order.")
-    args = parser.parse_args()
+    parser.add_argument("-d", "--delimiter", choices=['comma', 'tab', 'pipe', 'semi-colon'], help="The file delimiter of the input file")
+    parser.add_argument("-s", "--sorted", action = "store_true", help="Whether the output file should be sorted in ascending order.")
 
-    # Set the delimiter or use the default
-    if not args.delimiter:
-        delimiter = ','
-    elif args.delimiter == 'tab':
-        delimiter = '\t'
-    elif args.delimiter == 'pipe':
-        delimiter = '|'
-    elif args.delimiter == 'semi-colon':
-        delimiter = ';'
-    else:
-        print("Delimiter must be one of 'tab', 'pipe', or 'semi-colon'. This should have been caught by argparse: something may be wrong with that part of the code.")
-        exit()
+    return parser.parse_args()
+
+def extract_field_from_file(input_file, position, delimiter = ',', sorted = False):
+    """
+        Extracts the field at a given position from a delimited file, returning a List of unique values. Optionally sorts the field.
+
+        :param input_file: the file containing the field to be extracted; assumed to be a delimited file using one of the supported delimiters.
+        :param position: the position of the field to extract (zero-based)
+        :param delimiter: the file delimiter, defaulting to comma
+        :param sorted: whether the field should be sorted, defaulting to False
+        :returns: a List containing the unique values of the field (optionally sorted)
+    """
 
     # Open the file and extract the field
     try:
-        with open(args.input_file, 'rt') as f:
+        with open(input_file, 'rt') as f:
             csv_reader = csv.reader(f, delimiter = delimiter)
-            field = extract_delimited_field(csv_reader, args.field_position)
-    except OSError:
-        print(f'There was an error in opening or reading the file {args.input_file}. Make sure the file exists and is not corrupt.')
+            field = extract_delimited_field(csv_reader, position)
+    except IOError:
+        print(f'There was an error in opening or reading the file {input_file}. Make sure the file exists and is not corrupt.')
+        raise
 
     # Sort field if user desires
-    if args.sorted:
+    if sorted:
         field.sort()
 
-    # Writer output file
+    return field
+
+def write_field_to_file(output_file, field):
+    """
+        Writes the specified field (a List) to the specified file with rows terminated by newlines (here, '\n'). Does NOT add a trailing newline. Will overwrite any existing file without warning.
+
+        :param output_file: the file to write
+        :param field: the field to write (any single-dimension List-like object)
+        :returns: nothing
+    """
+
+    # Write output file
     # Wonky use of two f.write() is to remove trailing newline
     try:
-        with open(args.output_file, 'wt') as f:
+        with open(output_file, 'wt') as f:
             for value in field[0:-1]:
                 f.write(value + '\n')
             f.write(field[-1])
     except IOError:
-        print(f'There was an error writing data to the file {args.output_file}. Make sure the filename is valid and the program can write to the output folder.')
+        print(f'There was an error writing data to the file {output_file}. Make sure the filename is valid and the program can write to the output folder.')
+        raise
+
+def assign_delimiter_from_name(delimiter_name):
+    """
+        Returns the delimiter corresponding to the specified argument. Raises ValueError if argument is not a supported delimiter type.
+        
+        :param delimiter_name: the name corresponding to the delimiter to be selected. Must be one of 'comma', 'tab', 'pipe', or 'semi-colon'.
+        :returns: the delimiter value (i.e. ',', '\t', '|', or ';')
+    """
+
+    # Set the delimiter
+    # Allows 'comma' to be explicitly specified for users who want to do that
+    
+    if delimiter_name == 'comma':
+        delimiter = ','
+    elif delimiter_name == 'tab':
+        delimiter = '\t'
+    elif delimiter_name == 'pipe':
+        delimiter = '|'
+    elif delimiter_name == 'semi-colon':
+        delimiter = ';'
+    else:
+        print("Delimiter must be one of 'comma', 'tab', 'pipe', or 'semi-colon'. This should have been caught by argparse: something may be wrong with that part of the code.")
+        raise ValueError
+    
+    return delimiter
 
 if __name__ == '__main__':
-    extract_field_from_file()
+
+    # Parse CLI args
+    args = parse_cli_args()
+
+    # Assign delimiter (if specified) or set to comma
+    if not args.delimiter:
+        delimiter = ','
+    else:
+        delimiter = assign_delimiter_from_name(args.delimiter)
+    
+    # Extract field
+    field = extract_field_from_file(args.input_file, args.field_position, delimiter, args.sorted)
+
+    # Write field to file
+    write_field_to_file(args.output_file, field)
--- tests/extract-fields-unit-test.py
+++ tests/extract-fields-unit-test.py
@@ -4,9 +4,9 @@
 import csv
 import unittest as ut
 from unittest.case import TestCase
-from data_extract.extract_field import extract_delimited_field
+from data_extract.extract_field import extract_delimited_field, extract_field_from_file
 
-class TestFileDelimiters(ut.TestCase):
+class TestFileDelimiters(TestCase):
 
     # Tests whether the function can extract a field from a file
     # using one of the supported delimiters
@@ -38,7 +38,7 @@ class TestFileDelimiters(ut.TestCase):
             output_field = extract_delimited_field(csv_reader, 0)
         self.assertEqual(input_field, output_field)
 
-class TestPosition(ut.TestCase):
+class TestPosition(TestCase):
 
     # Tests whether the function can handle illegal position arguments
 
@@ -59,6 +59,22 @@ class TestPosition(ut.TestCase):
             csv_reader = csv.reader(input, delimiter = ',')
             with self.assertRaises(IndexError):
                 extract_delimited_field(csv_reader, 3) 
+
+class TestFileHandling(TestCase):
+
+    def test_file_input_error(self):
+        with self.assertRaises(IOError):
+            open('not/a/real/file/file.dat', 'rt')
+
+    def test_file_output_error(self):
+        with self.assertRaises(IOError):
+            open('not/a/real/file/file.dat', 'wt')
+
+class TestSorting(TestCase):
+
+    def test_sorted(self):
+        with open('tests/input/pokemon_test_data_input_csv.dat') as input:
+            output_field = extract_field_from_file()
 
 if __name__ == '__main__':
     ut.main()
