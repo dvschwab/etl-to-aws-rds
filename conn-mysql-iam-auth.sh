aa2bb4b0f73e123b68b80f102cc00baca946204c
David Schwab <75389902+dvschwab@users.noreply.github.com>
2021-09-13 20:23:13 -0400

    Update conn-mysql-iam-auth.sh
    Added comments and removed other private information

--- conn-mysql-iam-auth.sh
+++ conn-mysql-iam-auth.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 
-set -x
+# Script to connect to MySQL RDS instance using IAM authentication
+# Based on example at https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.IAMDBAuth.Connecting.AWSCLI.html
 
-IAMUSER="iam_user"
-HOSTNAME="data-science-project.cbzmjled6wkx.us-east-2.rds.amazonaws.com"
-REGION="us-east-2"
+# Uncomment to debug
+# set -x
 
+IAMUSER=your user name
+HOSTNAME=your host name
+REGION=your region
+
+# Generate the token using the AWS CLI
 TOKEN="$(aws rds generate-db-auth-token --hostname $HOSTNAME --port 3306 --username $IAMUSER --region $REGION)"
 
+# Connect using the MySQL client
+# Change path of AWS certificates as needed
 mysql -h $HOSTNAME --ssl-ca=/misc/global-bundle.pem --enable-cleartext-plugin --user=$IAMUSER --password=$TOKEN
